90167401dc489d35f23175e44846b67ee1ece6e8
Dave Shah <david.a.shah@gmail.com>
2019-01-12 19:31:19 -0500

    Add SwiftPhoenixClient

---
+++ FungusToast/Pods/Target Support Files/Pods-FungusToastTests/Pods-FungusToastTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FungusToastTests : NSObject
+@end
+@implementation PodsDummy_Pods_FungusToastTests
+@end
---
+++ FungusToast/Pods/Target Support Files/Pods-FungusToastUITests/Pods-FungusToastUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FungusToastUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FungusToastUITests
+@end
